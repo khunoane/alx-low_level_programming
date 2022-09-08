e5afe72604b3c78b86dd02538570665f5e149986
khunoane <ntsohak@gmail.com>
2022-09-08 10:32:33 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
@@ -9,6 +8,7 @@
  */
 int main(void)
 {
+   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",	
    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",       sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
    return (1);
 }
