9c995e90d3e48178586caeda74ca94be5ab6a523
khunoane <ntsohak@gmail.com>
2023-04-04 05:08:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,15 @@
 
 /**
  * main - Entry point
- * 
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
+
